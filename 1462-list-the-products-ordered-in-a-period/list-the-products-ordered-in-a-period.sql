9ab109630d805bcaec087696262a61ed0e3ab9c0
yashwanth5-gangu <yashwanthreddygangu@gmail.com>
2025-08-21 23:54:37 +0530

    Time: 649 ms (91.26%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -6,6 +6,6 @@ from products p1
 left join orders o1
  on p1.product_id=o1.product_id
  where o1.order_date  between '2020-02-01' and '2020-02-29' 
- group by p1.product_name
+ group by p1.product_name,o1.product_id
  having sum(o1.unit)>=100
  
