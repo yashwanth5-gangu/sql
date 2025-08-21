e369162ce2d447d54a9ed8ae4679d9dd901b094f
yashwanth5-gangu <yashwanthreddygangu@gmail.com>
2025-08-21 23:39:48 +0530

    Time: 689 ms (67.06%) | Memory: 0B (100.00%) - LeetSync

--- 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 select *
 from users
-where regexp_like(mail, '^[A-Za-z][A-Za-z0-9\_\.\-]*@leetcode[.]com$','c')
+where regexp_like(mail, '^[A-Za-z][A-Za-z0-9_.-]*@leetcode[.]com$','c')
