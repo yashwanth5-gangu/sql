8635d79ed08580c2240d811587b206a374fa3636
yashwanth5-gangu <yashwanthreddygangu@gmail.com>
2025-08-21 23:52:30 +0530

    Time: 724 ms (52.27%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+
+select p1.product_name,sum(o1.unit) as unit
+from products p1
+left join orders o1
+ on p1.product_id=o1.product_id
+ where o1.order_date  between '2020-02-01' and '2020-02-29' 
+ group by o1.product_id
+ having sum(o1.unit)>=100
+ 
