85bf1d0cff766e8ae6192eb919bff1791461006b
yashwanth5-gangu <yashwanthreddygangu@gmail.com>
2025-08-27 22:58:26 +0530

    Time: 675 ms (19.46%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,13 +1,10 @@
 # Write your MySQL query statement below
 select customer_id
 from customer 
-where product_key in(
-    select product_key
-    from product 
-)
+
 group by customer_id
 having count(distinct(product_key))=(
     select count(product_key)
     from product
-    
+
 )
