8f015e2a21a507523763f8c64f1977979f1daf9b
yashwanth5-gangu <yashwanthreddygangu@gmail.com>
2025-09-03 00:12:53 +0530

    Time: 643 ms (23.83%) | Memory: 0B (100.00%) - LeetSync

---
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+
+select round(sum(tiv_2016),2) as tiv_2016
+from insurance i1
+where (lat,lon) in(
+    select lat,lon
+    from insurance
+    group by lat,lon
+    having count(lat)=1
+
+)
+and tiv_2015 in(
+    select tiv_2015
+    from insurance
+    group by tiv_2015
+    having count(*)>1
+)
